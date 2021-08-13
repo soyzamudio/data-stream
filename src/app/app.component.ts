c64b358ea521938a3af211c4a70f93cb3d807d19
Jose <joseeduardozamudio@NA-LLA000254.local>
2021-08-12 23:37:36 -0500

    set up ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { GetAppData } from './store/app.actions';
+import { getData } from './store/app.selector';
+import { select, Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+import { AppState } from './store/app.reducer';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  data$: Observable<string> = this.store.pipe(select(getData));
   title = 'data-stream';
+  constructor(private store: Store<AppState>) {
+    this.store.dispatch(new GetAppData());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppEffects } from './store/app.effects';
+import { appReducer } from './store/app.reducer';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
+    EffectsModule.forRoot([AppEffects]),
+    StoreModule.forRoot(
+      {
+        app: appReducer,
+      },
+      {
+        runtimeChecks: {
+          strictStateImmutability: false,
+          strictActionImmutability: false,
+        },
+      })
   ],
   providers: [],
   bootstrap: [AppComponent]
