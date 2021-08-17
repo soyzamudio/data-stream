778226983c944b81f40e011ba7fdf62b4c6a1d7c
Jose <joseeduardozamudio@NA-LLA000254.local>
2021-08-16 22:40:14 -0500

    gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 export const environment = {
   production: true,
   pubKey: 'pub-c-83634a09-b64e-41cf-8b58-b694d32b8781',
-  subKey: 'sub-c-d078711c-fbef-11eb-bf4c-22908b043f7e',
+  subKey: 'sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe',
   sidemenu: [
     {
       image: 'https://i.ibb.co/6tKFLWG/home.png',
